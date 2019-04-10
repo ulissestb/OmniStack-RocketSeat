2ed6947a493f9cb92e690bf0f52d97ef4b188215
ulissestb <ulisses.melo.nascimento@gmail.com>
2019-04-10 14:07:08 -0300

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-jyj7u.mongodb.net/omnistack?retryWrites=true', {
+    useNewUrlParser: true
+});
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(require('./routes'));
+
+app.listen(3333);
