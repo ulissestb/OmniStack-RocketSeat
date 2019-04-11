f699e35acc1970c28fbe73a574fdc70073bd4ab9
ulissestb <ulisses.melo.nascimento@gmail.com>
2019-04-11 10:26:44 -0300

    added Procfile for Heroku

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');   
+const cors = require('cors');
 
 
 const app = express();
+
+app.use(cors());
+
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    })
+})
 
 mongoose.connect('mongodb+srv://omnistack:1234@cluster0-jyj7u.mongodb.net/omnistack?retryWrites=true', {
     useNewUrlParser: true
 });
+
+app.use((req, res, next) =>{
+    req.io = io;
+
+    return next();
+})
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
