dfcfed5f59566db3d5ce6051eba1ba6e71d09002
ulissestb <ulisses.melo.nascimento@gmail.com>
2019-04-12 16:24:51 -0300

    Continuando o frontend
    Adicionado página box
    conferir erro no cors.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Main from './pages/Main'
+import Routes from './routes'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Main />
+        <Routes />
       </div>
     );
   }
--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,15 +1,39 @@
 import React, {Component} from 'react'
+import api from '../../services/api'
+
+
 import './styles.css'
 import logo from '../../assets/logo.svg'
 
 export default class Main extends Component{
+    state = {
+        newBox: '',
+
+    }
+    
+    handleSubmit = async e => {
+        e.preventDefault();
+
+        const response = await api.post('boxes', {
+            title: this.state.newBox,
+        })
+
+        this.props.history.push(`/box/${response.data._id}`)
+    }
+
+    handleInputChange = e => {
+        this.setState({newBox: e.target.value}) 
+    }
+
     render(){
         return(
             <div id="main-container">
-                <form action="">
+                <form onSubmit={this.handleSubmit} action="">
                     <img src={logo} alt="logo" />
                     <input 
                         placeholder="criar um box"
+                        value={this.state.newBox}
+                        onChange={this.handleInputChange}
                     />
                     <button type="submit">Criar</button>
                 </form>
